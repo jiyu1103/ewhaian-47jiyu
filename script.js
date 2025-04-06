773692953770bdc3a340ee89a593c40d24a34201
jiyu <jiyu06103@naver.com>
2025-04-07 01:36:24 +0900

    js 색 변경 -> 클래스 변경

--- script.js
+++ script.js
@@ -1,46 +1,37 @@
 const apply = document.querySelector("#apply");
 const work = document.querySelector("#work");
-const ect = document.querySelector("#ect");
+const etc = document.querySelector("#etc");
 const applyContent = document.querySelector("#applyContent");
 const workContent = document.querySelector("#workContent");
 const etcContent = document.querySelector("#etcContent");
 
 function applyClick(){
+    apply.className = "button qs this";
+    work.className = "button qs";
+    etc.className = "button qs";
     applyContent.style.display = "block";
     workContent.style.display = "none";
     etcContent.style.display = "none";
-    apply.style.color = "black";
-    apply.style.backgroundColor = "white";
-    work.style.color = "white";
-    work.style.backgroundColor = "#1d1d1d";
-    etc.style.color = "white";
-    etc.style.backgroundColor = "#1d1d1d";
 }
 
 function workClick(){
+    apply.className = "button qs";
+    work.className = "button qs this";
+    etc.className = "button qs";
     applyContent.style.display = "none";
     workContent.style.display = "block";
     etcContent.style.display = "none";
-    apply.style.color = "white";
-    apply.style.backgroundColor = "#1d1d1d";
-    work.style.color = "black";
-    work.style.backgroundColor = "white";
-    etc.style.color = "white";
-    etc.style.backgroundColor = "#1d1d1d";
 }
-function etcClick(){
+
+function ectClick(){
+    apply.ClassName = "button qs";
+    work.ClassName = "button qs";
+    etc.ClassName = "button qs this";
     applyContent.style.display = "none";
     workContent.style.display = "none";
     etcContent.style.display = "block";
-    apply.style.color = "white";
-    apply.style.backgroundColor = "#1d1d1d";
-    work.style.color = "white";
-    work.style.backgroundColor = "#1d1d1d";
-    etc.style.color = "black";
-    etc.style.backgroundColor = "white";
 }
-
 
 apply.onclick = applyClick;
 work.onclick = workClick;
-etc.onclick = etcClick;
+etc.onclick = ectClick;
